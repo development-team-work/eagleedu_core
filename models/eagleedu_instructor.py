cec334dcce1c01201ea4a4e01197319a8f6a6991
Shaheen Hossain <rapidgrps@princegroup-bd.com>
2019-12-18 00:02:12 +0600

    intructor details

--- models/eagleedu_instructor.py
+++ models/eagleedu_instructor.py
@@ -17,11 +17,15 @@ class EagleeduInstructor(models.Model):
 
     ins_name = fields.Char(string="Instructor Name", required=True)
     instructor_id = fields.Char(string="Instructor ID No.", readonly=True )
-    ins_father_name = fields.Char(string="Instructor Father Name")
-    ins_mother_name = fields.Char(string="Instructor Mother Name")
+    ins_father_name = fields.Char(string="Father Name")
+    ins_mother_name = fields.Char(string="Mother Name")
+    ins_mobile_no = fields.Char(string="Mobile no.")
+    ins_email = fields.Char(string="Email Address")
+    ins_present_address = fields.Char(string="Present Address")
+    ins_permanent_address = fields.Char(string="Permanent Address")
     ins_image = fields.Binary(string="Instructor Image")
     ins_gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')],
                                 string='Gender', required=False, track_visibility='onchange',
-                                help="Your Gender is ")
+                                help="Your Gender is")
     ins_date_of_birth = fields.Date(string="Date Of birth", help="Enter your DOB")
 
