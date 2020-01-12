20f9635f4ce8779115b52471f4c02af618a209e6
Shaheen Hossain <rapidgrps@princegroup-bd.com>
2020-01-13 01:42:58 +0600

    trying to many2many group division

--- models/eagleedu_class.py
+++ models/eagleedu_class.py
@@ -11,9 +11,11 @@ class EagleeduClass(models.Model):
     name = fields.Char(string='Class', required=True, help="Enter the Name of the Class")
     class_id = fields.Many2one('eagleedu.class', string='Class', help="Enter the Name of the Class")
     section_id = fields.Many2one('eagleedu.section', string='Section Name', help="Enter the Name of the Section")
-    groupdivision_id = fields.Many2one('eagleedu.groupdivision', string='Group Division', help="Enter the Name of the Group division")
     roman_name = fields.Char(string='Roman Name ', help="Enter the Code of the Class")
+
+    groupdivision_id = fields.Many2one('eagleedu.groupdivision', string='Group Division', help="Enter the Name of the Group division")
+
     # section_ids=fields.Many2many('education.class.section',column2='level_ids',column1='section_ids',string='Sections')
-    # syllabus_ids = fields.One2many('education.syllabus', 'class_id')
+    # syllabus_ids = fields.One2many('education.syllabus', 'class_id') #
     # division_ids = fields.Many2many('education.division''class_id', 'class_id')
     # division_ids=fields.Many2many('education.division','class_dev_rel','division_ids','classes_ids')
--- models/eagleedu_groupdivision.py
+++ models/eagleedu_groupdivision.py
@@ -4,7 +4,30 @@ from eagle.exceptions import ValidationError
 from eagle import fields, models, api, _
 
 
-class EagleeduSection(models.Model):
+class EagleeduGroupdivision(models.Model):
     _name = 'eagleedu.groupdivision'
     _description = "Group Division "
     name = fields.Char(string='Group Division', help="Enter the Name of the Group Division")
+    groupdivisions_ids = fields.Many2many('eagleedu.groupdivision', 'eagleedu_groupdivision_rel', 'groupdivisions_ids')
+
+
+
+    # compulsory_for = fields.Many2many('education.class.history', 'education_syllabus_class_history_rel',
+    #                                   'compulsory_for', 'compulsory_subjects', 'compulsory for')
+
+
+    def view_groupdivisions(self):
+        """Return the list of current students in this class"""
+        self.ensure_one()
+        groupdivisions = self.env['eagleedu.groupdivision'].search([('class_id', '=', self.id)])
+        groupdivisions_list = groupdivisions.mapped('id')
+        return {
+            'domain': [('id', 'in', groupdivisions_list)],
+            'name': _('Group Division'),
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_model': 'eagleedu.groupdivision',
+            'view_id': False,
+            # 'context': {'default_classes_ids': self.id},
+            'type': 'ir.actions.act_window'
+        }
