84469a5167dfeb382b1455db801e3114e6380a83
Md. Shaheen Hossain <rapidgrps@princegroup-bd.com>
2020-02-05 00:02:19 +0600

    register menu rearrange

--- __manifest__.py
+++ __manifest__.py
@@ -18,7 +18,7 @@ This module for This is a e education manament system
             'views/eagleedu_student.xml',
             'views/eagleedu_instructor.xml',
             # 'views/eagleedu_main_menu.xml',
-            'views/eagleedu_class_division.xml',
+            #'views/eagleedu_class_division.xml',
             'views/eagleedu_academic_year.xml',
             'views/eagleedu_standard_class.xml',
             'views/eagleedu_class_section.xml',
